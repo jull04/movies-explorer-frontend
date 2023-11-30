b64a28d88defb4be55c242c1482d222a66e8a2d8
Julia Khabibova <julia.khabibova@mail.ru>
2023-12-01 00:04:15 +0500

    fixed mistakes

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,10 +4,10 @@ import './SearchForm.css';
 function SearchForm() {
   return (
     <section className='search'>
-      <div className='search__container'>
+      <form className='search__container' name='search' noValidate>
         <input className='search__input' placeholder='Фильм'></input>
         <button className='search__button' type='submit'>Поиск</button>
-      </div>
+      </form>
       <div className='search__filter-container'>
         <FilterCheckbox/>
       </div>
